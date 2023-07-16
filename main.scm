be6a99fbb19117dbc89003f496e4ef932873b582
kniffy <kniffy@sampler.fi>
2023-07-16 01:58:21 +0000

    figuring out IO

--- main.scm
+++ main.scm
@@ -1,15 +1,24 @@
 ; this is a basic text walking sim, related to the Lycosurian Mysteries,
 ; and meant to be played via telnet/ssh
+
 ; it was planned to be a promotional tie in with the album release, but
-; originally it was based on a python bbs package that is now deprecated;
-; it contained its own server daemon which is overkill for a project as
-; simple as this.
+; the decision was made to cancel, given the basis of the project was a
+; deprecated python bbs daemon package, not to mention efforts needed to
+; be focused on actually finishing the music.. 
+
+; so here we are a couple years later, maybe listen to the music while
+; playing this :^)
 
 ; when compiled and placed in /bin or similar, it is meant to be called
 ; as a user's login shell
 
-(define map
-  (make-vector 10 0))
+; https://tildas.org.
 
-(display map)
+(define area
+  (vector '1 '0 '1))
 
+(display area)
+(newline)
+
+(display "You're standing in front of some ruins \n")
+(display (read))
