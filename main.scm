1283831a0d4901a6108f5ac090c3378991082821
kniffy <kniffy@sampler.fi>
2023-07-16 07:59:28 +0000

    im dumb and cant grok getting user input yet :^)

--- main.scm
+++ main.scm
@@ -15,20 +15,46 @@
 ; https://tildas.org.
 
 (import (chicken io))	; we need read-line
+(import input-parse)
+(import simple-loops)
+(import srfi-1)
+(import linenoise)
+;(import srfi-13)	; maybe we need this in the future
 
+; set up and definitions
+; TODO: strings containing the descriptions, etc
 (define area
   (vector '1 '0 '1))
 
-;(display area)	; for testing
-;(newline)
-
 (display "You're standing in front of some ruins \n")
 (define userinput
-  (read-line (current-input-port) 10))
+  (read-line (current-input-port)))
 
-(if (string-ci=? "hi" userinput)
-  (display "you said hi"))
+(define userquit 0)
+
+(define actions
+  (list "quit" "exit"))
+
+; main loop here
+(do-while (not (string-ci=? "quit" userinput))
+  (read-text-line))
+
+;(if (find userinput actions)
+;  (display "you matched a thing")
+;  (display " you didnt"))
+
+;(do-forever
+;  (find userinput actions))
+;	  (if (string-ci=? "quit" userinput)
+;	    (exit)))
+
+	 ; (if (string-ci=? "hi" userinput)
+	  ;  (display "you said hi\n")
+	   ; (display "you didnt say hi\n")))
+
+
 
 ; we need to define the list of actions the user can do
 ; and read the users text:
-; north, south, east, west, xyzzy, look, help
+; north, south, east, west, xyzzy, look, help, quit
+; do we need a whole parser, or just a match in a lookup table?
