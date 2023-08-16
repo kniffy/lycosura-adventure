5c586e1f4cd9e8e9344119bb6bd39775ce8b65af
kniffy <kniffy@sampler.fi>
2023-08-16 08:26:39 +0000

    the most optimized we can do

--- build-dist.bash
+++ build-dist.bash
@@ -1,8 +1,8 @@
-#!/usr/local/bin/bash
+#!/usr/bin/env bash
 set -eu
 
 # for release, we want a tiny stripped
 # and optimized file, and packed zstd
-csc -o adv -O5 -strip main.scm
-zstd -19 -o adv.zst adv
+csc -o adv -O4 -fixnum-arithmetic -strip -vvv main.scm
+zstd --ultra -22 -o adv.zst adv
 rm adv
