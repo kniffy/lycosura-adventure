51d592a5aa610c7e63b263dbe2ebb85c57e75db6
kniffy <kniffy@sampler.fi>
2023-08-17 04:50:02 +0000

    strip debug info

--- build-dist.bash
+++ build-dist.bash
@@ -3,6 +3,6 @@ set -eu
 
 # for release, we want a tiny stripped
 # and optimized file, and packed zstd
-csc -o adv -O4 -fixnum-arithmetic -strip -vvv main.scm
+csc -o adv -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv main.scm
 zstd --ultra -22 -o adv.zst adv
 rm adv
