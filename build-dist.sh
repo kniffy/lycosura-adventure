396a624ab6278a5d0280b5abc53a5369c80abe3b
kniffy <kniffy@sampler.fi>
2023-08-18 15:40:15 +0000

    building is more complex it turns out

--- build-dist.sh
+++ build-dist.sh
@@ -2,6 +2,9 @@
 
 # for release, we want a tiny stripped
 # and optimized file, and packed zstd
-csc -o adv -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv main.scm
+csc -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv -c text.scm
+csc -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv -c main.scm
+csc -o adv -vvv main.o text.o
 zstd --ultra -22 -o adv.zst adv
-rm adv
+rm adv main.o text.o
+
