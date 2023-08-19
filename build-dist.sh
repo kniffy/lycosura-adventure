e2b138def847eb0def9ea021f449ad2aa46cb818
kniffy <kniffy@sampler.fi>
2023-08-19 09:10:42 +0000

    typo

--- build-dist.sh
+++ build-dist.sh
@@ -3,7 +3,7 @@
 # for release, we want a tiny stripped
 # and optimized file, and packed zstd
 echo 'did you strip the pos command, and the load text.scm thing'
-echo 'if this is gold, you're using the wrong build steps'
+echo 'if this is gold, youre using the wrong build steps'
 sleep 10
 csc -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv -c text.scm
 csc -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv -c main.scm
