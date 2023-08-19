a778ef8f527d7e01f168f9abd482203c5dd2c714
kniffy <kniffy@sampler.fi>
2023-08-19 09:09:04 +0000

    some user-checks :^)

--- build-dist.sh
+++ build-dist.sh
@@ -2,9 +2,11 @@
 
 # for release, we want a tiny stripped
 # and optimized file, and packed zstd
+echo 'did you strip the pos command, and the load text.scm thing'
+echo 'if this is gold, you're using the wrong build steps'
+sleep 10
 csc -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv -c text.scm
 csc -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv -c main.scm
-csc -o adv -vvv main.o text.o
+csc -o adv -O4 -fixnum-arithmetic -strip -debug-level 0 -vvv main.o text.o
 zstd --ultra -22 -o adv.zst adv
 rm adv main.o text.o
-
