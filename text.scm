01fc88a3f36617b422b01d5f7698c981033692bf
kniffy <kniffy+github@sampler.fi>
2024-10-31 15:54:43 -0500

    proper multiline, slight 80col fixes

--- text.scm
+++ text.scm
@@ -2,14 +2,13 @@
 ; in the map array definition. should be pretty fast and efficient; we will
 ; not be reading from disk
 
+(declare (unit text))
+
 ; should we format to 80col?
 
 ; there is not really a nice way to do multi-line strings, this maybe breaks
 ; on other Schemes, if in doubt use Chicken
 
-(declare (unit text))
-
-; not entirely sure why this huge block actually works..
 (define banner #<<END
 DEVELOPMENT VERSION CIRCA NOV. 2024
 
@@ -35,103 +34,176 @@ END
 ; variable names here are arbitrary, but we'll try to follow the convention:
 ; each tile area is assigned a number 1-64, refer to the chart for positioning
 ; extra text syntax is text<num>-<b>
-(define text01 "You're at the start of a winding road, you can see it goes uphill, but there
+(define text01 #<<END
+You're at the start of a winding road, you can see it goes uphill, but there
 are trees blocking all view of whats beyond.
 
 Why even go down this? It doesn't seem scary, just pointless. Turning over
 rocks can never lead to good, as far as I know, but I'm just gone.
-")
 
-(define text02 "All the paths I could have gone down, yet I'm on this one somehow; what were
+END
+)
+
+(define text02 #<<END
+All the paths I could have gone down, yet I'm on this one somehow; what were
 down those roads?
-")
 
-(define text03 "A turn is in the distance, still can't see beyond though.
-")
+END
+)
 
-(define text04 "You're at the curve, the road continues south.. Maybe turn back?
-")
+(define text03 #<<END
+A turn is in the distance, still can't see beyond though.
 
-(define text05 "Plans never seem to work out, but we'll get where we're going, one step at a
+END
+)
+
+(define text04 #<<END
+You're at the curve, the road continues south.. Maybe turn back?
+
+END
+)
+
+(define text05 #<<END
+Plans never seem to work out, but we'll get where we're going, one step at a
 time. There's freedom in that, moving forward, casting aside weights.
-")
 
-(define text06 "There's so much to dig up from the past - sparkling rubies or horrifying shit
+END
+)
+
+(define text06 #<<END
+There's so much to dig up from the past - sparkling rubies or horrifying shit
 that ought to be forgotten. Was there a sign back there? How far have we gone?
-")
 
-(define text07 "A great emptiness is here, where it was once meaningful. What might've been,
+END
+)
+
+(define text07 #<<END
+A great emptiness is here, where it was once meaningful. What might've been,
 or what was, here we are now.
-")
 
-(define text08 "You read somewhere the nature of the animal sacrifices here.. instead of
+END
+)
+
+(define text08 #<<END
+You read somewhere the nature of the animal sacrifices here.. instead of
 outright killing, limbs would be cut off - why would the Mistress make it so?
-")
 
-(define text09 "A brilliant blue butterfly suddenly flies past. Is it actually so empty? Or is
+END
+)
+
+(define text09 #<<END
+A brilliant blue butterfly suddenly flies past. Is it actually so empty? Or is
 only the mind empty. You're rounding a bend now, still no sight at whats ahead
-")
 
-(define text10 "How many lives have you lived? How many were here in the past - the infinite
+END
+)
+
+(define text10 #<<END
+How many lives have you lived? How many were here in the past - the infinite
 place.
-")
 
-(define text11 "You're seeking peace - freedom from chaos as much as worry, but how is peace
+END
+)
+
+(define text11 #<<END
+You're seeking peace - freedom from chaos as much as worry, but how is peace
 under stone?
-")
 
-(define text12 "It's very hot today, and humid.
-")
+END
+)
 
-(define text13 "It doesn't work like that. 10 more days til I'm one year older, in the valley
+(define text12 #<<END
+It's very hot today, and humid.
+
+END
+)
+
+(define text13 #<<END
+It doesn't work like that. 10 more days til I'm one year older, in the valley
 - will the abyss look back
-")
 
-(define text14 "In the past it's like I was asleep, totally unaware of being so, but I can't
+END
+)
+
+(define text14 #<<END
+In the past it's like I was asleep, totally unaware of being so, but I can't
 even say when waking up happened, or if it even did happen, so I must still be
 asleep. I'm just gone.
-")
 
-(define text15 "A bit hungry now.
-")
+END
+)
 
-(define text16 "What is the Mistress' real name? If only one could still join the Mysteries
+(define text15 #<<END
+A bit hungry now.
+
+END
+)
+
+(define text16 #<<END
+What is the Mistress' real name? If only one could still join the Mysteries
 and find out
-")
 
-(define text17 "How did we end up at this path? Love or something less, maybe. No one else
+END
+)
+
+(define text17 #<<END
+How did we end up at this path? Love or something less, maybe. No one else
 will be here.
 
 Running away or finding something more - is there a difference
-")
 
-(define text18 "Why did you come to this place? What are you doing here? I just had to run
+END
+)
+
+(define text18 #<<END
+Why did you come to this place? What are you doing here? I just had to run
 away, which worked for a while, but there's that void.
-")
 
-(define text19 "A friend of mine told me once: he had a connection with a girl in the early
-days of the internet. It was their habit to talk every day, til one day he didn't get online
-for whatever reason.. he never saw her online again.
-")
+END
+)
 
-(define text20 "Nobody knows when I'm being serious nobody wants you here, go home. Beyond
+(define text19 #<<END
+A friend of mine told me once: he had a connection with a girl in the early
+days of the internet. It was their habit to talk every day, til one day he
+didn't get online for whatever reason.. he never saw her online again.
+
+END
+)
+
+(define text20 #<<END
+Nobody knows when I'm being serious nobody wants you here, go home. Beyond
 the traditional obscurity and weariness in my life, it's mystery.
 
 Til I see you again
-")
 
-(define text21 "A couple of birds fly by overhead. You still can't see much - how long is
-this road? A bit unnerving with just trees all around, but it's calm and serene here.
-")
+END
+)
 
-(define text22 "Where did we leave the car? Or was there one? Difficult listening at first;
+(define text21 #<<END
+A couple of birds fly by overhead. You still can't see much - how long is this
+road? A bit unnerving with just trees all around, but it's calm and serene
+here.
+
+END
+)
+
+(define text22 #<<END
+Where did we leave the car? Or was there one? Difficult listening at first;
 so, timeless.
-")
 
-(define text23 "You might think you know what you don't. I bet your brain is broken because
+END
+)
+
+(define text23 #<<END
+You might think you know what you don't. I bet your brain is broken because
 you're a person and your brain will try to do what which we can't.
-")
 
-(define text24 "Is this place a graveyard? Hopefully just ruins..
-")
+END
+)
+
+(define text24 #<<END
+Is this place a graveyard? Hopefully just ruins..
+
+END
+)
 
